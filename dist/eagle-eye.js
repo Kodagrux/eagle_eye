741d085b093a68b8ba72ea00a2dd2b31eaa34f56
Arvid Bräne <arvidbrane@gmail.com>
2017-03-11 18:43:16 +0100

    Changed GPIO repo

--- dist/eagle-eye.js
+++ dist/eagle-eye.js
@@ -1,3 +1,14 @@
 "use strict";
 
+var _rpio = require("rpio");
+
+var _rpio2 = _interopRequireDefault(_rpio);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//var rpio = require('rpio');
+
 console.log("arvid");
+
+_rpio2["default"].open(11, _rpio2["default"].INPUT);
+console.log("Pin 11 is currently set " + (_rpio2["default"].read(11) ? "high" : "low"));
--- src/eagle-eye.js
+++ src/eagle-eye.js
@@ -1,1 +1,7 @@
-console.log("arvid");
+import gpio from "rpi-gpio";
+
+
+gpio.on("change", function(channel, value) {
+    console.log("Channel " + channel + " value is now " + value);
+});
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH); //pin 7, not gpio 7!!
